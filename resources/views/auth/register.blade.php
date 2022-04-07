fa27e216e2bf57ebabe34272b825108e75324af5
Dorian Beaumaikers <dorianbeaumaikers@gmail.com>
2022-04-07 20:30:10 +0200

    Validate.js

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,35 +1,190 @@
 @extends('template.index')
 
 @section('main')
-<main class="mx-auto container py-5" style="width: 40%;">
+<main class="container p-2">
 
     <h1>Register</h1>
 
-    <form action="{{ route('register') }}" method="post">
+    <form action="{{ route('register') }}" method="post" id="main" novalidate>
         @csrf
 
-        <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control" name="name" id="name">
+        <div class="form-group mb-3">
+            <label class="col-sm-2 control-label" for="name">Nom</label>
+            <div class="col-sm-5">
+                <input id="name" class="form-control" type="text" placeholder="Nom" name="name">
+            </div>
+            <div class="col-sm-5 messages">
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">Email address</label>
-            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <div class="form-group mb-3">
+            <label class="col-sm-2 control-label" for="email">Email</label>
+            <div class="col-sm-5">
+                <input id="email" class="form-control" type="email" placeholder="Email" name="email">
+            </div>
+            <div class="col-sm-5 messages"></div>
         </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" name="password" id="password">
-        </div>
-        <div class="mb-3">
-            <label for="password_confirmation" class="form-label">Confirm password</label>
-            <input type="password" class="form-control" name="password_confirmation" id="password_confirmation">
-        </div>
-        <button type="submit" class="btn btn-primary me-3">Register</button>
+        <div class="form-group mb-3">
+            <label class="col-sm-2 control-label" for="password">Mot de passe</label>
+            <div class="col-sm-5">
+              <input id="password" class="form-control" type="password" placeholder="Mot de passe" name="password">
+            </div>
+            <div class="col-sm-5 messages"></div>
+          </div>
+          <div class="form-group mb-3">
+            <label class="col-sm-2 control-label" for="password_confirmation">Confirmer le mot de passe</label>
+            <div class="col-sm-5">
+              <input id="password_confirmation" class="form-control" type="password" placeholder="Confirmer le mot de passe" name="password_confirmation">
+            </div>
+            <div class="col-sm-5 messages"></div>
+          </div>
+        <button type="submit" class="btn btn-primary me-3">Enregistrer</button>
         <a href="users" class="btn btn-primary me-3">Annuler</a>
 
     </form>
 </main>
+@endsection
+
+@section('script')
+    <script>
+        (function() {
+            // These are the constraints used to validate the form
+            var constraints = {
+                email: {
+                    // Email is required
+                    presence: true,
+                    // and must be an email (duh)
+                    email: true,
+                },
+                name: {
+                    // You need to pick a username too
+                    presence: true,
+                    format: {
+                        // We don't allow anything that a-z and 0-9
+                        pattern: "[a-z0-9]+",
+                        // but we don't care if the username is uppercase or lowercase
+                        flags: "i",
+                        message: "^Le nom ne peut contenir que a-z et 0-9"
+                    }
+                },
+                password: {
+                    // Password is also required
+                    presence: true,
+                    // And must be at least 5 characters long
+                    format: {
+                        // We don't allow anything that a-z and 0-9
+                        pattern: "(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}",
+                        message: "^Le mot de passe doit contenir au min. 8 caractères, une minuscule, une majuscule et un caractère spécial."
+                    }
+                },
+                "password_confirmation": {
+                    // You need to confirm your password
+                    presence: true,
+                    // and it needs to be equal to the other password
+                    equality: {
+                        attribute: "password",
+                        message: "^The passwords does not match"
+                    }
+                }
+            };
+
+            // Hook up the form so we can prevent it from being posted
+            var form = document.querySelector("form#main");
+            form.addEventListener("submit", function(ev) {
+                ev.preventDefault();
+                handleFormSubmit(form);
+            });
+
+            // Hook up the inputs to validate on the fly
+            var inputs = document.querySelectorAll("input, textarea, select")
+            for (var i = 0; i < inputs.length; ++i) {
+                inputs.item(i).addEventListener("change", function(ev) {
+                    var errors = validate(form, constraints) || {};
+                    showErrorsForInput(this, errors[this.name])
+                });
+            }
+
+            function handleFormSubmit(form, input) {
+                // validate the form against the constraints
+                var errors = validate(form, constraints) ? validate(form, constraints) : null;
+                // then we update the form to reflect the results
+                showErrors(form, errors || {});
+                if (!errors) {
+                    submitForm(form);
+                }
+            }
+
+            // Updates the inputs with the validation errors
+            function showErrors(form, errors) {
+                // We loop through all the inputs and show the errors for that input
+                _.each(form.querySelectorAll("input[name], select[name]"), function(input) {
+                    // Since the errors can be null if no errors were found we need to handle
+                    // that
+                    if(input.name != "_token" && input.name != "_method"){
+                        showErrorsForInput(input, errors && errors[input.name]);
+                    }
+                });
+            }
+
+            // Shows the errors for a specific input
+            function showErrorsForInput(input, errors) {
+                // This is the root of the input
+                var formGroup = closestParent(input.parentNode, "form-group");
+                    // Find where the error messages will be insert into
+                var messages = formGroup.querySelector(".messages");
+                // First we remove any old messages and resets the classes
+                resetFormGroup(formGroup);
+                // If we have errors
+                if (errors) {
+                    // we first mark the group has having errors
+                    formGroup.classList.add("has-error");
+                    // then we append all the errors
+                    _.each(errors, function(error) {
+                        addError(messages, error);
+                    });
+                } else {
+                    // otherwise we simply mark it as success
+                    formGroup.classList.add("has-success");
+                }
+            }
+
+            // Recusively finds the closest parent that has the specified class
+            function closestParent(child, className) {
+                if (!child || child == document) {
+                    return null;
+                }
+                if (child.classList.contains(className)) {
+                    return child;
+                } else {
+                    return closestParent(child.parentNode, className);
+                }
+            }
+
+            function resetFormGroup(formGroup) {
+                // Remove the success and error classes
+                formGroup.classList.remove("has-error");
+                formGroup.classList.remove("has-success");
+                // and remove any old messages
+                _.each(formGroup.querySelectorAll(".help-block.error"), function(el) {
+                    el.parentNode.removeChild(el);
+                });
+            }
+
+            // Adds the specified error with the following markup
+            // <p class="help-block error">[message]</p>
+            function addError(messages, error) {
+                var block = document.createElement("p");
+                block.classList.add("help-block");
+                block.classList.add("error");
+                block.innerText = error;
+                messages.appendChild(block);
+            }
+
+            function submitForm(form) {
+                console.log("Test");
+                form.submit();
+            }
+        })();
+    </script>
 @endsection
 
 
--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
-    <link rel="stylesheet" href="{{asset("css/app.css")}}">
+    <link rel="stylesheet" href="{{asset("public/css/app.css")}}">
 </head>
 
 <body class="antialiased">
@@ -58,6 +58,9 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/validate.js/0.13.1/validate.min.js"></script>
+    
     
 
     @yield('script')
